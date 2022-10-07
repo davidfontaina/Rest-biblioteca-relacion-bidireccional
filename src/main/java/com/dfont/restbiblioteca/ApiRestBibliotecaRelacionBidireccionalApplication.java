751876f73065d73364873d294f20aa0250d11978
Fontaina David <david.fontaina@gmail.com>
2022-10-07 00:18:00 -0300

    se crea el proyecto con el STS

---
+++ src/main/java/com/dfont/restbiblioteca/ApiRestBibliotecaRelacionBidireccionalApplication.java
@@ -0,0 +1,13 @@
+package com.dfont.restbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBibliotecaRelacionBidireccionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBibliotecaRelacionBidireccionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dfont/restbiblioteca/ApiRestBibliotecaRelacionBidireccionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfont.restbiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBibliotecaRelacionBidireccionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
